911507f91e440aa5c0f793cb34eed204ef62e485
Abhishek Singh <Abhishek.singh@singleinterface.com>
2022-06-02 15:17:31 +0530

    npm install react-native-device-info

--- ConnectMobileApp/src/utility/Config_File.js
+++ ConnectMobileApp/src/utility/Config_File.js
@@ -7,3 +7,4 @@ export const API_URL_DEV = 'https://test-chat.starify.co';
  * API_URL_STAGING is for Testing/Staging purpose.
  */
 export const API_URL_STAGING = 'https://test-chat-1.starify.co';
+
