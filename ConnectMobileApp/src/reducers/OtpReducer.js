7c31963e591fd128a75fbcf318ee489e1a06e07c
AjayDevAj <ajay.bharti@singleinterface.com>
2022-06-06 11:12:00 +0530

    OTP Screen file commit.

---
+++ ConnectMobileApp/src/reducers/OtpReducer.js
@@ -0,0 +1,19 @@
+
+import { CONSTANT } from "../utility/Constant";
+import { combineReducers } from "redux";
+
+const otpDataReducer = (state = '', action) => {
+
+    if(action.type == CONSTANT.OTP_DATA_SUCCESS) {
+        return action.payload;
+
+    }
+    if (action.type == CONSTANT.OTP_DATA_ERROR) {
+        return action.payload;
+    }
+    return state;
+
+}
+export default otpDataReducer;
+
+
