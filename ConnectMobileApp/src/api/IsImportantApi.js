531d04a02934d0a347d1bcc0702ef5384aaad8ae
AjayDevAj <ajay.bharti@singleinterface.com>
2022-06-16 12:30:05 +0530

    Updata code

--- ConnectMobileApp/src/api/IsImportantApi.js
+++ ConnectMobileApp/src/api/IsImportantApi.js
@@ -10,32 +10,6 @@ export const  getIsImportantData = async (conversation_id, is_important) => {
     /****** get token from store asy class... */
     const token_Value = await getOtpResponse(otpResponse_Storage_Key)
 
-
-    const bodyRawData = {
-        "conversation_id": conversation_id, //d77fde6b41494837f42b8e26
-        "is_important": is_important, //1
-    };
-
-    console.log('Message importent mark ',bodyRawData)
-    var api_url = API_URL_STAGING + '/message/mark-as-important';
-    var headers = {
-        Authorization:
-        `Bearer ${token_Value.token}`,
-        'Content-Type': 'application/json',
-      }
-
-    const response = await fetch(api_url, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(bodyRawData),
-    })
-
-    const data = response.json()
-    console.log('isImportant API data : ',data)
-   
-    if (response.status > 400) {
-        throw new Error(data.errors)
-
     /****** constant defined to get login from an api ****/
     const bodyData = new FormData();
     bodyData.append('conversation_id', "d77fde6b41494837f42b8e26")
