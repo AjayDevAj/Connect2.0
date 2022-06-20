63482ea68e33f6b3d8a5fc2c3d097d82259d59b8
diksha-singleinterface <diksha.sahu@singleinterface.com>
2022-06-20 19:18:40 +0530

    Updated Card manual data

--- ConnectMobileApp/src/Card/CardRowOne.js
+++ ConnectMobileApp/src/Card/CardRowOne.js
@@ -8,14 +8,14 @@ import CardIcon from './CardIcon';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const CardRowOne = ({ logo, name, location, is_important }) => {
+const CardRowOne = ({ logo, name, location, isImportant }) => {
     return (
         <View style={ cardStyles.cardRowOne }>
             <View style={ cardStyles.cardLogoNameContainer}>
                 <CardIcon logo={logo} />
             
                 <Text style={ cardStyles.cardName }>{ name }</Text>
-                {is_important > 0 && <Icon name="star-border" size={16} color="#FFAA00" style={{ opacity: 1, marginLeft: 5, marginTop: 2 }} />}
+                {isImportant > 0 && <Icon name="star-border" size={16} color="#FFAA00" style={{ opacity: 1, marginLeft: 5, marginTop: 2 }} />}
             </View>
             
             <CardLocation location={location} />
--- ConnectMobileApp/src/Card/CardRowTwo.js
+++ ConnectMobileApp/src/Card/CardRowTwo.js
@@ -8,16 +8,28 @@ import cardStyles from './styles/CardStylesheet';
 import CardTime from './CardTime';
 
 const CardRowTwo = ({ assigned, message, time, unread, status }) => {
-    // unread = 1
+    unread = 1
+    const msgLength = message.length;
     return (
         <View style={ cardStyles.cardRowTwo }>
             <View style={{ alignSelf: 'flex-start' }}></View>
             {  message != '' ? 
-                    (unread > 0 && status == 'open') ? 
-                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', width: '76%' }}>
-                            <Text style={ cardStyles.unreadChatMessage } numberOfLines={1} ellipsizeMode='tail'>{ message }</Text>
-                            <Text style={ cardStyles.unreadCountDesign } > { unread }</Text> 
+                    (unread > 0 && status == 'open') ? (
+                        <View>
+                            {msgLength > 31 ? (
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', width: '68%' }}>
+                                    <Text style={ cardStyles.unreadChatMessage } numberOfLines={1} ellipsizeMode='tail'>{ message }</Text>
+                                    <Text style={ cardStyles.unreadCountDesign } > { unread }</Text> 
+                                </View>
+                            ) : (
+                                <View style={{ flexDirection: 'row',  }}>
+                                    <Text style={ cardStyles.unreadChatMessage }>{ message } </Text>
+                                    <Text style={ cardStyles.unreadCountDesignLessThan31 } > { unread }</Text> 
+                                </View>
+                            )}
+                            
                         </View>
+                    )
                     :
                         <Text style={ cardStyles.chatMessage }>{ message }</Text>
                 : 
