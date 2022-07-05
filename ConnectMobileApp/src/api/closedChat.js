43e3d67f2dcf2d6bd2e884504236c9630dee7021
Abhishek Singh <Abhishek.singh@singleinterface.com>
2022-07-05 16:37:34 +0530

    close chat api call

---
+++ ConnectMobileApp/src/api/closedChat.js
@@ -0,0 +1,48 @@
+
+
+import { API_URL_STAGING } from '../utility/Config_File'
+import { CONSTANT } from '../utility/Constant';
+import { otpResponse_Storage_Key } from '../utility/Constant'
+import { getOtpResponse } from '../utility/StorageClass'
+
+
+const closedChat = async (chat_status,conversation_id) => {
+
+    /****** get token from store asy class... */
+    const token_Value = await getOtpResponse(otpResponse_Storage_Key)
+
+    var api_url = API_URL_STAGING + '/message/open-close-message';
+    var headers = {
+        Authorization:
+        `Bearer ${token_Value.token}`,
+        'Content-Type': 'application/json',
+    }
+
+    const bodyRawData = {
+        "chat_status": chat_status,
+        "conversation_id": is_important,
+    };
+
+    const response = await fetch(api_url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(bodyRawData),
+    });
+
+    const data = response.json()
+    console.log('isImportant API data : ', data)
+
+    if (response.status > 400) {
+        // if (response.status == 401) {
+        //     navigation.navigate(NavigationString.LOGIN)
+        // }
+        // else {
+        //     throw new Error(data.errors);
+        // }
+        throw new Error(data.errors);
+    }
+    return data;
+}
+
+export { closedChat }
+
