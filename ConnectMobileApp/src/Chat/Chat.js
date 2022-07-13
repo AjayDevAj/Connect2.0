21457dca81815ed00e49b33d346fd0686c952795
Shafhad Khan <shafhad.khan@singleinterface.com>
2022-07-13 19:07:24 +0530

    changed checked-box library

--- ConnectMobileApp/src/Chat/Chat.js
+++ ConnectMobileApp/src/Chat/Chat.js
@@ -51,6 +51,8 @@ const Chat = ({navigation ,Route  }) => {
   const [pageNo, setPageNo] = useState(1);
   const [totalChatPageCount, setTotalChatPageCount] = useState(1);
 
+  
+
   const menuHandler = () => {
     // console.log('Menu Handler');
      // alert('Menu Handler');
