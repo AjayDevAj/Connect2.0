0c0cb60ab509577914ea67aaab6c476068994997
diksha-singleinterface <diksha.sahu@singleinterface.com>
2022-07-21 12:08:55 +0530

    Update Customer.js

--- ConnectMobileApp/src/Customer/Customer.js
+++ ConnectMobileApp/src/Customer/Customer.js
@@ -47,6 +47,7 @@
    const menuHandler = () => {
      //  navigation.navigate(navigationString.Customer);
      //  navigation.goBack();
+     navigation.openDrawer();
      setShowPurchaseForm(false);
    };
  
